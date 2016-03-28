6bd2297e80c1a91498c049e9eb43fd405e10b9a0
Lesther Vega <lvega@americavoice.com>
2016-03-27 22:44:10 -0600

    init project

---
+++ app/src/main/java/gt/android/basicmaps/MainActivity.java
@@ -0,0 +1,60 @@
+package gt.android.basicmaps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we
+     * just add a marker near Africa.
+     */
+    @Override
+    public void onMapReady(GoogleMap map) {
+        // Other supported types include: MAP_TYPE_NORMAL, MAP_TYPE_SATELLITE
+        // MAP_TYPE_TERRAIN, MAP_TYPE_HYBRID and MAP_TYPE_NONE
+        //map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+
+        LatLng mapCenter = new LatLng(14.643047, -90.513083);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(mapCenter, 13));
+
+        // You can customize the marker image using images bundled with
+        // your app, or dynamically generated bitmaps.
+        map.addMarker(new MarkerOptions()
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_location_city_black_24px))
+                .anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
+                .position(mapCenter)
+                .title("Palacio de la Cultura"));
+
+        CameraPosition
+                cameraPosition = CameraPosition.builder()
+                .target(mapCenter)
+                .zoom(13)
+                .bearing(90)
+                .build();
+
+        // Animate the change in camera view over 2 seconds
+        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition),
+                2000, null);
+    }
+}
